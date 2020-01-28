3c87773ad00e6662a35d70869c8131e3bc75c025
Sabrina Becker <sbecka8@gmail.com>
2020-01-28 13:47:20 -0500

    Add filters for query params and each one returns movies correctly

--- server.js
+++ server.js
@@ -6,22 +6,41 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello, world!');
-});
-
 // endpoint movie with query params: genre, country, avg_vote
 
 
 
 app.get('/movie', function handleGetMovie(req, res) {
     const { genre, country, avg_vote } = req.query;
-    let movies 
+    let movies = MOVIEDEX;
     if (genre) {
+        movies = movies.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase())
+        );
+    };
 
+    if (country) {
+        movies = movies.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        );
+    };
+
+    if (avg_vote) {
+        let vote = Number(avg_vote);
+
+        if (avg_vote > 10 || avg_vote < 0) {
+            res.status(400).send('Average vote must be between 0 and 10.');
+        };        
+        console.log(vote);
+        
+        movies = movies.filter(movie => 
+            movie.avg_vote >= vote
+        );
+    };
+
+    res.json(movies);
     }
-    
-    }
+
 );
 
 const PORT = 8000;
